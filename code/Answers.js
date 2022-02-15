769222fef4a084c2061d24ed913757c780a48c0e
MissRosy <88013528+MissRosy@users.noreply.github.com>
2022-02-14 21:11:49 -0600

    Add files via upload

---
+++ code/Answers.js
@@ -0,0 +1,9 @@
+// Respuestas
+const accessCode1 = "UNO";
+
+function QuestionsReact() {
+    // VAriable
+    fill("Black");
+    textSize(20);
+    text("Objeto inicial que tienes para sobrevivir", 870,460);
+}
